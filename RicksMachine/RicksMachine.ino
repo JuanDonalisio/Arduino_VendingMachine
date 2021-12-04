9beb0af9683ed864e944e8c7a2b577c013354e7c
leispini <38335384+JuanDonalisio@users.noreply.github.com>
2021-12-04 11:39:56 -0300

    Speaker

--- RicksMachine/RicksMachine.ino
+++ RicksMachine/RicksMachine.ino
@@ -1,9 +1,22 @@
+#include <SoftwareSerial.h>
+#include <SD.h>
+#include <SPI.h>
+#define SD_ChipSelectPin 53
+#include <TMRpcm.h>
+
+TMRpcm tmrpcm;
 void setup() {
   // put your setup code here, to run once:
+  tmrpcm.speakerPin = 5;
+  tmrpcm.quality(1);
+  tmrpcm.setVolume(50);
 
+  if (!SD.begin(SD_ChipSelectPin)) {
+    Serial.println("SD fail");
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  tmrpcm.play((char *)"rick.wav");
 }
