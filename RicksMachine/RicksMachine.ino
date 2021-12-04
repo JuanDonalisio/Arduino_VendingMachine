81e5ada20aa6256fa4f2e7227cc3c89f40989ee2
leispini <38335384+JuanDonalisio@users.noreply.github.com>
2021-12-04 11:45:40 -0300

    Wifi & Server

--- RicksMachine/RicksMachine.ino
+++ RicksMachine/RicksMachine.ino
@@ -1,3 +1,4 @@
+#include <Servo.h>
 #include <SoftwareSerial.h>
 #include <SD.h>
 #include <SPI.h>
@@ -5,6 +6,37 @@
 #include <TMRpcm.h>
 
 TMRpcm tmrpcm;
+int motor_a_pin= 3;
+int motor_a_pin= 6;
+int motor_a_pin= 7;
+
+void conexionServidor() {
+  Serial1.println("AT+CIPSTART=\"TCP\",\"192.168.100.10\",4000");
+  if (Serial1.find("OK"))
+  {
+    Serial.println("Conectado al servidor");
+  }
+  else
+  {
+    Serial.println("Error conectandose al servidor, intentando nuevamente");
+    conectarServidor();
+  }
+}
+
+void conexionWifi() {
+  // SSID y Clave del WiFi
+  Serial1.println("AT+CWJAP=\"Fibertel WiFi997 2.4GHz\",\"panqueques\"");
+  if (Serial1.find("OK"))
+  {
+    Serial.println("Conectado a WiFi");
+  }
+  else
+  {
+    Serial.println("Error conectandose al WiFi, intentando nuevamente");
+    conectarWifi();
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   tmrpcm.speakerPin = 5;
